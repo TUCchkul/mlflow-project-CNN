4f612ac44c80588e7bada8115631a35b1d6e8b74
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-05-19 08:12:32 +0200

    update get_data

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -20,12 +20,16 @@ logging.basicConfig(
 def main(config_path):
     ## read config files
     config = read_yaml(config_path)
+    #print(config)#output->{'URL': {'source_url': 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip', 'local_dir': 'data', 'data_file': 'data.zip'}}
     URL=config["URL"]["source_url"]
+    #print(URL)#output->url link
     local_dir=config["URL"]["local_dir"]
+    #print(local_dir)# output->data
     create_directories([local_dir])
     data_file=config["URL"]["data_file"]
+    #print(data_file)#output->data.zip
     data_file_path=os.path.join(local_dir, data_file)
-    
+    #print(data_file_path)# output->data\data.zip
     if not os.path.isfile(data_file_path):
         logging.info(f"Downloading started.....")
         filename, headers=req.urlretrieve(URL, data_file_path)
